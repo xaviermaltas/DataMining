4da1c7e1b1fd23486e17c04c43f337cfec686380
Xavier Maltas <xavi.maltass@gmail.com>
2021-04-30 01:10:39 +0200

    Deleting all the stuff

--- PRA1/MD-PRA1.R
+++
@@ -1,8 +0,0 @@
-# https://cran.r-project.org/web/packages/ggplot2/index.html
-if (!require('ggplot2')) install.packages('ggplot2'); library('ggplot2')
-
-# https://cran.r-project.org/web/packages/dplyr/index.html
-if (!require('dplyr')) install.packages('dplyr'); library('dplyr')
-
-print("Hello World!")
-
