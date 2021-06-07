8088110db8adb43923b990113f2e9320548e28ea
Xavier Maltas T14 <xavi.maltass@gmail.com>
2021-06-08 00:51:48 +0200

    PRA2 - CSVs and code file

---
+++ PRA2/code.R
@@ -0,0 +1,12 @@
+#https://cran.r-project.org/web/packages/ggplot2/index.html
+if (!require('ggplot2')) install.packages('ggplot2'); library('ggplot2')
+
+# https://cran.r-project.org/web/packages/dplyr/index.html
+if (!require('dplyr')) install.packages('dplyr'); library('dplyr')
+
+if (!require(cluster)) install.packages(cluster);library(cluster)
+if(!require(MVA)) install.packages("MVA"); library(MVA)
+if(!require(tidyr)) install.packages("tidyr");library(tidyr)
+if(!require(cluster)) install.packages("cluster"); library(cluster)
+if(!require(plyr)) install.packages("plyr"); library(plyr)
+if(!require(readr)) install.packages("readr"); library(readr)
